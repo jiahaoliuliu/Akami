0fcacd0593b31ba579fa9f505b40b26fb8c34893
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-07-10 20:49:03 +0400

    Added back button to the activity

--- app/src/main/java/com/jiahaoliuliu/akami/ui/MonthlyTransactionsActivity.java
+++ app/src/main/java/com/jiahaoliuliu/akami/ui/MonthlyTransactionsActivity.java
@@ -2,6 +2,7 @@ package com.jiahaoliuliu.akami.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.jiahaoliuliu.akami.R;
 
@@ -11,5 +12,20 @@ public class MonthlyTransactionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_transactions);
+
+        // Displaying the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Capture the back action
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
