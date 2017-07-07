3bb1035c3ad32085f7650f7cd270d353463e7a54
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2017-07-07 11:52:04 +0400

    Moved the list of companies to the model

--- app/src/main/java/com/jiahaoliuliu/akami/expenseslist/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/akami/expenseslist/MainActivity.java
@@ -67,38 +67,29 @@ public class MainActivity extends BaseActivity
     private RecyclerView mTransactionsRecyclerView;
     private TextView mNoSmsTextView;
 
-    // Internal variables
-    private Context mContext;
-
     // The expenses per month
     private HashMap<Long, Float> mTransactionsPerMonth;
     private List<ITransactions> mTransactionsList;
     private TransactionsListAdapter mTransactionsListAdapter;
     private LinearLayoutManager mLinearLayoutManager;
 
-    // The list of companies
-    private Map<String, Company> mCompaniesMap;
-
     // The header date formatter. This has to be static in order to be used by the adapter
     public static SimpleDateFormat sHeaderDateFormatter = new SimpleDateFormat(HEADER_DATE_FORMAT);
     // The month of the first element shown in the header
     private long mFirstElementMonthlyKey;
 
-    private ExpensesListPresenter mExpensesListPresenter;
+    private ExpensesListPresenter mPesenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Set the base presenter for the base view and for this view
         // This should be done before the method onCreated because
         // it is linked on the lifecycle of the application
-        mExpensesListPresenter = (ExpensesListPresenter) getPresenter();
-        setPresenter(mExpensesListPresenter);
+        mPesenter = (ExpensesListPresenter) getPresenter();
+        setPresenter(mPesenter);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Set variables
-        this.mContext = this;
 
         // Link the views
         mHeaderDateTextView = (TextView) findViewById(R.id.header_date_text_view);
@@ -110,9 +101,6 @@ public class MainActivity extends BaseActivity
         mTransactionsRecyclerView.setLayoutManager(mLinearLayoutManager);
 
         mNoSmsTextView = (TextView) findViewById(R.id.no_sms_text_view);
-
-        // Create the list of companies
-        mCompaniesMap = generateComapniesList();
 
         // Parse the list of transactions from the device
         parseTransactions();
@@ -180,7 +168,8 @@ public class MainActivity extends BaseActivity
                     }
                 }
             });
-            mTransactionsListAdapter = new TransactionsListAdapter(mContext, mTransactionsList, mCompaniesMap, mTransactionsPerMonth);
+            mTransactionsListAdapter = new TransactionsListAdapter(mContext, mTransactionsList,
+                    mPesenter.getCompaniesList(), mTransactionsPerMonth);
             mTransactionsRecyclerView.setAdapter(mTransactionsListAdapter);
 
             // Disable the no sms view
@@ -241,165 +230,6 @@ public class MainActivity extends BaseActivity
             mTransactionsPerMonth);
         startActivity(startMonthlyExpensesActivityIntent);
         return;
-    }
-
-    // TODO: Use database instead
-    private Map<String, Company> generateComapniesList() {
-        Map<String, Company> companiesMap = new HashMap<>();
-
-        // Carrefour Mall of Emirates
-        String companyId = "CARREFOUR-MOE,DUBAI-AE";
-        Company company = new Company(companyId, "Carrefour");
-        company.setImageResourceId(R.drawable.logo_carrefour);
-        companiesMap.put(companyId, company);
-
-        // Carrefour Marina
-        companyId = "CARREFOUR MARKET MAR,DUBAI-AE";
-        company = new Company(companyId, "Carrefour");
-        company.setImageResourceId(R.drawable.logo_carrefour);
-        companiesMap.put(companyId, company);
-
-        // Carrefour TeCom
-        companyId = "CARREFOUR MARKET TEC,DUBAI-AE";
-        company = new Company(companyId, "Carrefour");
-        company.setImageResourceId(R.drawable.logo_carrefour);
-        companiesMap.put(companyId, company);
-
-        // Al merkaz
-        companyId = "AL MERKAZ,DUBAI-AE";
-        company = new Company(companyId, "Al Merkaz");
-        company.setImageResourceId(R.drawable.logo_almerkaz);
-        companiesMap.put(companyId, company);
-
-        // Capella club
-        companyId = "CAPELLA CLUB MARINE,DUBAI-AE";
-        company = new Company(companyId, "Capella club");
-        companiesMap.put(companyId, company);
-
-        // Spinney
-        companyId = "SPINNEYS DUBAI LLC 9,DUBAI-AE";
-        company = new Company(companyId, "Spinneys");
-        company.setImageResourceId(R.drawable.logo_spinneys);
-        companiesMap.put(companyId, company);
-
-        companyId = "SPINNEYS DUBAI LLC,DUBAI-AE";
-        company = new Company(companyId, "Spinneys");
-        company.setImageResourceId(R.drawable.logo_spinneys);
-        companiesMap.put(companyId, company);
-
-        // Media one hotel
-        companyId = "MEDIA ONE HOTEL,DUBAI-AE";
-        company = new Company(companyId, "Media one hotel");
-        company.setImageResourceId(R.drawable.logo_media_one_hotel);
-        companiesMap.put(companyId, company);
-
-        // Vapiano
-        companyId = "VAP HOSPITALITY -731,DUBAI-AE";
-        company = new Company(companyId, "Vapiano");
-        company.setImageResourceId(R.drawable.logo_vapiano);
-        companiesMap.put(companyId, company);
-
-        // RTA
-        companyId = "RTA-TOM,DUBAI-AE";
-        company = new Company(companyId, "RTA");
-        company.setImageResourceId(R.drawable.logo_rta);
-        companiesMap.put(companyId, company);
-
-        // RTA
-        companyId = "ROAD & TRANSPORT AUT,DUBAI-AE";
-        company = new Company(companyId, "RTA");
-        company.setImageResourceId(R.drawable.logo_rta);
-        companiesMap.put(companyId, company);
-
-        // Waitrose
-        companyId = "WAITROSE-FINEFARE,DUBAI-AE";
-        company = new Company(companyId, "Waitrose");
-        company.setImageResourceId(R.drawable.logo_waitrose);
-        companiesMap.put(companyId, company);
-
-        // Eton institute
-        companyId = "ETON EDUCATIONAL INS,DUBAI-AE";
-        company = new Company(companyId, "Eton institue");
-        company.setImageResourceId(R.drawable.logo_eton_institute);
-        companiesMap.put(companyId, company);
-
-        // Q Mart Mini Market JLT
-        companyId = "Q MART MINI MARKET J,DUBAI-AE";
-        company = new Company(companyId, "Q Mart Mini Market JLT");
-        companiesMap.put(companyId, company);
-
-        // Blue mart
-        companyId = "EXPRESS BLUE MART SU,DUBAI-AE";
-        company = new Company(companyId, "Express Blue Mart");
-        companiesMap.put(companyId, company);
-
-        // Indigo rent a car
-        companyId = "INDIGO RENT A CAR DM,DUBAI-AE";
-        company = new Company(companyId, "Indigo rent a car");
-        companiesMap.put(companyId, company);
-
-        // Borders
-        companyId = "BORDERS-IBN BATT-251,DUBAI-AE";
-        company = new Company(companyId, "Borders");
-        companiesMap.put(companyId, company);
-
-        // Decathlong
-        companyId = "DECATHLON,DUBAI-AE";
-        company = new Company(companyId, "Decathlon");
-        companiesMap.put(companyId, company);
-
-        // Alphamed Pharma
-        companyId = "ALPHAMED IBN SIN-714,DUBAI-AE";
-        company = new Company(companyId, "Alphamed Pharma");
-        companiesMap.put(companyId, company);
-
-        // Adidas
-        companyId = "ADIDAS EMERGING -261,DUBAI-AE";
-        company = new Company(companyId, "Adidas");
-        companiesMap.put(companyId, company);
-
-        // Panmeas Jewellery
-        companyId = "PANMEAS JEWELLERY LL,ABU DHABI-AE";
-        company = new Company(companyId, "Panmeas Jewellery");
-        companiesMap.put(companyId, company);
-
-        // Enoc
-        companyId = "ENOC SITE 4010 CSTOR,DUBAI-AE";
-        company = new Company(companyId, "Enoc");
-        companiesMap.put(companyId, company);
-
-        // Bershka
-        companyId = "BERSHKA-FAMA TRADING,DUBAI-AE";
-        company = new Company(companyId, "Bershka");
-        companiesMap.put(companyId, company);
-
-        // Careem
-        companyId = "CAREEM NETWORKS FZ L,DUBAI-AE";
-        company = new Company(companyId, "Careem");
-        company.setImageResourceId(R.drawable.logo_careem);
-        companiesMap.put(companyId, company);
-
-        // Zoom
-        companyId = "SITE 6549 ZOOM MARKE,DUBAI-AE";
-        company = new Company(companyId, "Zoom");
-        companiesMap.put(companyId, company);
-
-        // Air france
-        companyId = "AIR FRANCE,ROISSY CDG CE-FR";
-        company = new Company(companyId, "Air France");
-        companiesMap.put(companyId, company);
-
-        // Microless Computer
-        companyId = "MICROLESS DMCC,DUBAI-AE";
-        company = new Company(companyId, "Microless computer");
-        companiesMap.put(companyId, company);
-
-        // Express VPN
-        companyId = "EXPRESS VPN,310-601-8492-US";
-        company = new Company(companyId, "Express VPN");
-        companiesMap.put(companyId, company);
-
-        return companiesMap;
     }
 
     // TODO: Use Dagger instead
--- app/src/main/java/com/jiahaoliuliu/akami/modelviewpresenter/BaseActivity.java
+++ app/src/main/java/com/jiahaoliuliu/akami/modelviewpresenter/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.jiahaoliuliu.akami.modelviewpresenter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseActivity extends AppCompatActivity {
 
     protected BasePresenter mBasePresenter;
+    protected Context mContext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mContext = this;
         mBasePresenter.onViewCreated();
     }
 
