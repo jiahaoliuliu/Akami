9af435975950a5ab47333b0f2f4cd2342e986f81
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2017-07-14 20:03:32 +0400

    Set the base for the dagger injection

---
+++ app/src/main/java/com/jiahaoliuliu/akami/dependencyinjection/AppComponent.java
@@ -0,0 +1,20 @@
+package com.jiahaoliuliu.akami.dependencyinjection;
+
+import com.jiahaoliuliu.akami.transactionslist.MainActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by jiahaoliuliu on 7/7/17.
+ */
+
+@Singleton
+@Component(modules = MvpModule.class)
+public interface AppComponent {
+
+    // Views -> To inject the views, the activity should be used because the variables to be
+    // injected are in the activities, not in the views itself
+    void inject(MainActivity mainActivity);
+}
