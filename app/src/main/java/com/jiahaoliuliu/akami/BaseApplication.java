9005e156742d798a62e1ecc2c0737a0c06de8012
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2017-08-31 00:26:04 +0400

    Updated Robolectric version. Added AssertJ

--- app/src/main/java/com/jiahaoliuliu/akami/BaseApplication.java
+++ app/src/main/java/com/jiahaoliuliu/akami/BaseApplication.java
@@ -15,7 +15,6 @@ public abstract class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         sInstance = this;
     }
 
--- app/src/test/java/com/jiahaoliuliu/akami/test/transactionslist/MainActivityTest.java
+++ app/src/test/java/com/jiahaoliuliu/akami/test/transactionslist/MainActivityTest.java
@@ -1,12 +1,8 @@
 package com.jiahaoliuliu.akami.test.transactionslist;
 
-import android.app.Application;
-
 import com.jiahaoliuliu.akami.BaseApplication;
 import com.jiahaoliuliu.akami.BuildConfig;
-import com.jiahaoliuliu.akami.MainApplication;
 import com.jiahaoliuliu.akami.MainTestApplication;
-import com.jiahaoliuliu.akami.di.AppTestComponent;
 import com.jiahaoliuliu.akami.transactionslist.MainActivity;
 
 import org.junit.Before;
@@ -14,18 +10,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertTrue;
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by jiahaoliuliu on 7/15/17.
  */
+//@RunWith(RobolectricTestRunner.class)
+//@Config(constants = BuildConfig.class, sdk = 21, packageName = "com.jiahaoliuliu.akami",
+//        application = MainTestApplication.class)
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21, packageName = "com.jiahaoliuliu.akami",
-        application = MainTestApplication.class)
+@Config(constants = BuildConfig.class, application = MainTestApplication.class)
 public class MainActivityTest {
 
 
@@ -34,7 +31,8 @@ public class MainActivityTest {
     @Before
     public void setup() {
         (((MainTestApplication) BaseApplication.getInstance())
-                .getComponent()).inject(this);
+                .getComponent())
+                .inject(this);
 
         mMainActivity = Robolectric.buildActivity(MainActivity.class).setup().get();
     }
