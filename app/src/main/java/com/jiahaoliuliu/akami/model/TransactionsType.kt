33cd9d8cee057e89ae52a19d648bb10429f54bc2
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2018-04-21 14:14:29 +0400

    Converted enum Type to TransactionsType

---
+++ app/src/main/java/com/jiahaoliuliu/akami/model/TransactionsType.kt
@@ -0,0 +1,11 @@
+package com.jiahaoliuliu.akami.model
+
+// TODO: Set colour
+// TODO: Create the super class Transactions which contains the follow classes
+// The type of the transactions
+enum class TransactionsType {
+    EXPENSE,
+    WITHDRAW,
+    SALARY,
+    UNKNOWN
+}
